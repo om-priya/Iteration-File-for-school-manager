4fd5a57f36e268d16758059b9cc1941390c31153
Om Priya <ompriya18153789@gmail.com>
2024-02-19 16:56:56 +0530

    Iteration file

---
+++ dump.js
@@ -0,0 +1,19 @@
+// {
+//     "iterationFor": "Teacher",
+//     "iterationInfo": {
+//       "iterationId": 2,
+//       "iterationName": "Teacher Happy Path",
+//       "iterationDescription": "This iteration will check the happy paths for teacher through which one can check the functionality of teacher"
+//     },
+//     "routes": [
+//       "SignUp Teacher",
+//       "Login Super Admin",
+//       "Get All Principal",
+//       "Approve Principal",
+//       "Logout Request",
+//       "Login Principal",
+//       "",
+//       "Logout Request",
+//       "Tear Down Test Db"
+//     ]
+// },
